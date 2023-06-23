ec831f08437a2e184bafd85036f3caebb6d4da67
VicencioDx <81316815+VicencioDx@users.noreply.github.com>
2023-06-23 02:31:27 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,25 +1,28 @@
 const fs = require('fs');
 
 function readConfigFile(fileName) {
-    const data = fs.readFileSync(fileName, 'utf8').toString().split("\n");
-    return data.reduce((config, line) => {
-        const [key, value] = line.split("=");
-        if (key && value) {
-            config[key] = value;
-        }
-        return config;
-    }, {})
+  const data = fs.readFileSync(fileName, 'utf8').toString().split('\n');
+  return data.reduce((config, line) => {
+    const [key, value] = line.split('=');
+    if (key && value) {
+      config[key] = value;
+    }
+    return config;
+  }, {});
 }
 
-const Kafka = require("node-rdkafka");
-const config = readConfigFile("client.properties");
-config["group.id"] = "node-group";
+const Kafka = require('node-rdkafka');
+const config = readConfigFile('client.properties');
+config['group.id'] = 'node-group';
 
-const consumer = new Kafka.KafkaConsumer(config, {"auto.offset.reset": "earliest" });
-consumer.connect();
-consumer.on("ready", () => {
-    consumer.subscribe(["topic_0"]);
-    consumer.consume();
-}).on("data", (message) => {
-    console.log("Consumed message", message);
+const consumer = new Kafka.KafkaConsumer(config, { 'auto.offset.reset': 'earliest' });
+
+consumer.on('ready', () => {
+  consumer.subscribe(['topic_0']);
+  consumer.consume();
+}).on('data', (message) => {
+  const data = message.value.toString();
+  console.log('Consumed message:', data);
 });
+
+consumer.connect();
